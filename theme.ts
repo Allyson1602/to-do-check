fa5c78c299c3aa5866b2d416caa3fbc7b44fe72c
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-03-06 18:04:09 -0300

    (style): define theme to button

--- theme.ts
+++ theme.ts
@@ -1,6 +1,28 @@
 import { extendTheme } from "native-base";
 
-const newTheme = {};
-const theme = extendTheme({ colors: newTheme });
+const newTheme = {
+  components: {
+    Button: {
+      variants: {
+        solid: {
+          bg: "#8A3FFC",
+          borderRadius: 5,
+          _text: {
+            fontWeight: "500",
+          },
+        },
+        outline: {
+          borderColor: "#8A3FFC",
+          _text: {
+            color: "#8A3FFC",
+            fontWeight: "500",
+          },
+        },
+      },
+    },
+  },
+};
+
+const theme = extendTheme(newTheme);
 
 export default theme;
