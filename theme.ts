adade51f3fdcc2561f5b66f162e61afc7217841d
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-03-17 18:40:30 -0300

    (feat): add poppins font

--- theme.ts
+++ theme.ts
@@ -25,39 +25,11 @@ const newTheme = {
       },
     },
   },
-  // fontConfig: {
-  //   Poppins: {
-  //     100: {
-  //       normal: "Poppins_100Thin",
-  //     },
-  //     200: {
-  //       normal: "Poppins_200ExtraLight",
-  //     },
-  //     300: {
-  //       normal: "Poppins_300Light",
-  //     },
-  //     400: {
-  //       normal: "Poppins_400Regular",
-  //     },
-  //     500: {
-  //       normal: "Poppins_500Medium",
-  //     },
-  //     600: {
-  //       normal: "Poppins_600SemiBold",
-  //     },
-  //     700: {
-  //       normal: "Poppins_700Bold",
-  //     },
-  //     900: {
-  //       normal: "Poppins_900Black",
-  //     },
-  //   },
-  // },
-  // fonts: {
-  //   heading: "Poppins_400Regular",
-  //   body: "Poppins_400Regular",
-  //   mono: "Poppins_400Regular",
-  // },
+  fonts: {
+    heading: 'Poppins-Regular',
+    body: 'Poppins-Regular',
+    mono: 'Poppins-Regular',
+  },
 };
 
 const theme = extendTheme(newTheme);
